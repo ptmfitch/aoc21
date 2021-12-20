aca553d729ce89e946a0a8a92a884edfcf5a18f3
Peter Fitch <ptmfitch@users.noreply.github.com>
2021-12-20 17:44:59 +0000

    day 12 parts 1 and 2 complete

---
+++ solutions/12/part2.js
@@ -0,0 +1,43 @@
+[{$project: {
+  _id: 0,
+  connection: [
+   {
+    $arrayElemAt: [
+     {
+      $split: [
+       '$lines',
+       '-'
+      ]
+     },
+     0
+    ]
+   },
+   {
+    $arrayElemAt: [
+     {
+      $split: [
+       '$lines',
+       '-'
+      ]
+     },
+     1
+    ]
+   }
+  ]
+ }}, {$group: {
+  _id: 'null',
+  connections: {
+   $push: '$connection'
+  }
+ }}, {$project: {
+  _id: 0,
+  result: {
+   $function: {
+    body: 'function(connections) {\n        let connectionDict = {}\n        let paths = []\n        for (let c of connections) {\n          if (connectionDict[c[0]]) {\n            connectionDict[c[0]].push(c[1])\n          } else {\n            connectionDict[c[0]] = [c[1]]\n          }\n          if (connectionDict[c[1]]) {\n            connectionDict[c[1]].push(c[0])\n          } else {\n            connectionDict[c[1]] = [c[0]]\n          } \n        }\n        function pathfind(cur, acc, doubleVisit) {\n          if (acc[cur]) {\n            acc[cur]++\n          } else {\n            acc[cur] = 1\n          }\n          if (cur.toLowerCase() == cur && acc[cur] == 2) {\n            doubleVisit = true\n          }\n          if (cur == \'end\') {\n            paths.push(acc)\n            return\n          }\n          for (let c of connectionDict[cur]) {\n            if (c.toLowerCase() == c && c in acc) {\n              if (c == \'start\' || doubleVisit) {\n                continue\n              }\n            }\n            pathfind(c, Object.assign({}, acc), doubleVisit)\n          }\n        }\n        pathfind(\'start\', {}, false)\n        return paths.length\n      }',
+    args: [
+     '$connections'
+    ],
+    lang: 'js'
+   }
+  }
+ }}]
