427dd7be5978b7b563c7a87d39168cef83249db1
Peter Fitch <ptmfitch@users.noreply.github.com>
2022-11-28 13:01:01 +0000

    tidied up solutions for day 1

--- solutions/01/part1.js
+++ solutions/01/part1.js
@@ -1,61 +1,72 @@
-[{$group: {
- _id: null,
- depths: {
-  $push: '$$ROOT'
- }
-}}, {$project: {
- _id: 0,
- depths: {
-  $map: {
-   input: '$depths',
-   as: 'this',
-   'in': {
-    i: {
-     $indexOfArray: [
-      '$depths',
-      '$$this'
-     ]
+[
+  {
+    $group: {
+      _id: null,
+      depths: {
+        $push: "$line",
+      },
     },
-    o: '$$this'
-   }
-  }
- }
-}}, {$project: {
- depths: {
-  $filter: {
-   input: '$depths',
-   as: 'this',
-   cond: {
-    $cond: {
-     'if': {
-      $eq: [
-       '$$this.i',
-       0
-      ]
-     },
-     then: false,
-     'else': {
-      $gt: [
-       '$$this.o.depth',
-       {
-        $arrayElemAt: [
-         '$depths.o.depth',
-         {
-          $subtract: [
-           '$$this.i',
-           1
-          ]
-         }
-        ]
-       }
-      ]
-     }
-    }
-   }
-  }
- }
-}}, {$project: {
- result: {
-  $size: '$depths'
- }
-}}]
+  },
+  {
+    $project: {
+      _id: 0,
+      depths: {
+        $map: {
+          input: {
+            $range: [
+              0,
+              {
+                $size: "$depths",
+              },
+            ],
+          },
+          as: "i",
+          in: {
+            i: "$$i",
+            depth: {
+              $toInt: { $arrayElemAt: ["$depths", "$$i"] },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      depths: {
+        $filter: {
+          input: "$depths",
+          as: "this",
+          cond: {
+            $cond: {
+              if: {
+                $eq: ["$$this.i", 0],
+              },
+              then: false,
+              else: {
+                $gt: [
+                  "$$this.depth",
+                  {
+                    $arrayElemAt: [
+                      "$depths.depth",
+                      {
+                        $subtract: ["$$this.i", 1],
+                      },
+                    ],
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      res: {
+        $size: "$depths",
+      },
+    },
+  },
+]
--- solutions/01/part2.js
+++ solutions/01/part2.js
@@ -1,111 +1,116 @@
-[{$group: {
- _id: null,
- depths: {
-  $push: '$$ROOT'
- }
-}}, {$project: {
- _id: 0,
- depths: {
-  $map: {
-   input: '$depths',
-   as: 'this',
-   'in': {
-    i: {
-     $indexOfArray: [
-      '$depths',
-      '$$this'
-     ]
+[
+  {
+    $group: {
+      _id: null,
+      depths: {
+        $push: "$line",
+      },
     },
-    o: '$$this'
-   }
-  }
- }
-}}, {$set: {
- depths: {
-  $map: {
-   input: '$depths',
-   as: 'this',
-   'in': {
-    $cond: {
-     'if': {
-      $lt: [
-       '$$this.i',
-       2
-      ]
-     },
-     then: {
-      i: '$$this.i',
-      sum: 0
-     },
-     'else': {
-      i: '$$this.i',
-      sum: {
-       $add: [
-        '$$this.o.depth',
-        {
-         $arrayElemAt: [
-          '$depths.o.depth',
-          {
-           $subtract: [
-            '$$this.i',
-            1
-           ]
-          }
-         ]
+  },
+  {
+    $project: {
+      _id: 0,
+      depths: {
+        $map: {
+          input: {
+            $range: [
+              0,
+              {
+                $size: "$depths",
+              },
+            ],
+          },
+          as: "i",
+          in: {
+            i: "$$i",
+            depth: {
+              $toInt: { $arrayElemAt: ["$depths", "$$i"] },
+            },
+          },
         },
-        {
-         $arrayElemAt: [
-          '$depths.o.depth',
-          {
-           $subtract: [
-            '$$this.i',
-            2
-           ]
-          }
-         ]
-        }
-       ]
-      }
-     }
-    }
-   }
-  }
- }
-}}, {$set: {
- depths: {
-  $filter: {
-   input: '$depths',
-   as: 'this',
-   cond: {
-    $and: [
-     {
-      $gte: [
-       '$$this.i',
-       3
-      ]
-     },
-     {
-      $gt: [
-       '$$this.sum',
-       {
-        $arrayElemAt: [
-         '$depths.sum',
-         {
-          $subtract: [
-           '$$this.i',
-           1
-          ]
-         }
-        ]
-       }
-      ]
-     }
-    ]
-   }
-  }
- }
-}}, {$project: {
- result: {
-  $size: '$depths'
- }
-}}]
+      },
+    },
+  },
+  {
+    $set: {
+      depths: {
+        $map: {
+          input: "$depths",
+          as: "this",
+          in: {
+            $cond: {
+              if: {
+                $lt: ["$$this.i", 2],
+              },
+              then: {
+                i: "$$this.i",
+                sum: 0,
+              },
+              else: {
+                i: "$$this.i",
+                sum: {
+                  $add: [
+                    "$$this.depth",
+                    {
+                      $arrayElemAt: [
+                        "$depths.depth",
+                        {
+                          $subtract: ["$$this.i", 1],
+                        },
+                      ],
+                    },
+                    {
+                      $arrayElemAt: [
+                        "$depths.depth",
+                        {
+                          $subtract: ["$$this.i", 2],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      depths: {
+        $filter: {
+          input: "$depths",
+          as: "this",
+          cond: {
+            $and: [
+              {
+                $gte: ["$$this.i", 3],
+              },
+              {
+                $gt: [
+                  "$$this.sum",
+                  {
+                    $arrayElemAt: [
+                      "$depths.sum",
+                      {
+                        $subtract: ["$$this.i", 1],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      result: {
+        $size: "$depths",
+      },
+    },
+  },
+]
